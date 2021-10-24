acf366d7c808d6f3168c100c0dfab4acd98b3913
Esther Glazer <esthermalkaglazer@gmail.com>
2021-10-24 15:43:36 -0400

    I fixed some small stuff

--- src/payrollProgram/PayStubPrinter.java
+++ src/payrollProgram/PayStubPrinter.java
@@ -3,19 +3,22 @@ package payrollProgram;
 import java.time.LocalDate;
 import java.io.PrintWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class PayStubPrinter {
 
 	public PayStubPrinter() {
 	}
 
-	public PayStubPrinter(Employee emp, Address address, Pay pay, LocalDate date) {
+	public PayStubPrinter(Employee emp, Address address, Pay pay, LocalDate date) throws FileNotFoundException {
 		File file = new File(emp.getName() + "_" + date + ".txt");
 		PrintWriter printWriter = new PrintWriter(file);
 		printWriter.println(date);
 		printWriter.println(emp.getName());
 		printWriter.println(emp.getAddress());
-		printWriter.println(pay.getPay());
+		printWriter.println("Gross Pay: " + pay.getGrossPay());
+		printWriter.println("Net Pay: " + pay.getNetPay());
+		
 		printWriter.close();
 	}
 }
